1078a89c77281d440d3fc4bed752b91c0c15065a
dalikim <dalikim@parti.xyz>
2019-08-27 16:13:33 +0900

    최초

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: '툴킷',
+  description: '더나은 민주주의를 위한 툴킷',
+  themeConfig: {
+    logo: '/assets/img/parti_ci.png',
+    sidebar: [
+      {
+        title: '목차',
+        collapsable: true,
+        children: [
+          ['/', '소개'],
+          ['/campaigner.html', '캠페이너 인생 게임'],
+        ]
+      }
+    ]
+  }
+}
