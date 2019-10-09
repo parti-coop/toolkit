4d3729c8ab797fa8ad815e36d23cbd6ecd8f68d7
rest515 <rest515@gmail.com>
2019-10-09 15:56:28 +0900

    add soft groups contents

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,7 @@ module.exports = {
     ],
     sidebar: [
       {
-        title: '커뮤니티',
+        title: '커뮤니티 가이드',
         collapsable: false,
         children: [
           ['/community/community101.html', '커뮤니티 101'],
@@ -18,15 +18,16 @@ module.exports = {
         ]
       },
       {
-        title: '캠페인',
+        title: '캠페인 가이드',
         collapsable: false,
         children: [
           ['/campaign/campaign101.html', '캠페인 101'],
           ['/campaign/campaigner.html', '캠페이너 인생게임'],
         ]
       },
-      ['http://demosx.org/', '시민 참여 플랫폼'],
-      ['https://townhall.kr/toolkits', '빠띠 타운홀 툴킷'],
+      ['http://demosx.org/', '시민 참여 플랫폼 가이드'],
+      ['/soft/groups.html', '빠띠 그룹스 가이드'],
+      ['https://townhall.kr/toolkits', '빠띠 타운홀 가이드'],
       ['/tips.html', '기타 유용한 팁'],
       ['/lab.html', '빠띠의 실험들'],
       ['mailto:help@parti.xyz', '문의하기'], 
