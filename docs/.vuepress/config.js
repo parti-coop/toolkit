4f7ae2b8ac5e53ab5cc3119cd4ed42f4752d8328
SeungGu Jung <54617148+ssinkjung@users.noreply.github.com>
2020-04-01 08:35:04 +0900

    사이드바에 커뮤니티행동강령 세우기를 추가합니다

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,6 +15,7 @@ module.exports = {
         collapsable: false,
         children: [
           ["/community/community101", "소셜이슈 커뮤니티 101"],
+          ["/community/coc", "커뮤니티 행동강령 세우기"],
           ["/community/decision-making", "협력적 의사결정 툴킷"],
           ["/community/org101", "항해하는 조직 101"],
           ["/community/diy-party101", "DIY 정당 101"]
